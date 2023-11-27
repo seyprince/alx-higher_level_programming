888f7c1c03db515dbc8cf12010d3d955aa0bb2e8
SEY-prince <princesey@outlook.com>
2023-11-27 08:20:21 +0000

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-for al in range(97, 123):
-    print("{:c}".format(al), end='')
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def print_last_digit(number):
-    if number < 0:
-        last = (-number % 10)
-    elif number >= 0:
-        last = number % 10
-    print("{:d}".format(last), end="")
-    return (last)
